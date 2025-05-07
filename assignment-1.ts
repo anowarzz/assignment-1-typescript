d848f7e44a8c900d4fe373d0eb36bd4eece0cbe0
Anowar Hosen <anowarhosen444@gmail.com>
2025-05-07 23:18:53 +0600

    problem 2 solved

--- assignment-1.ts
+++ assignment-1.ts
@@ -11,4 +11,21 @@ console.log(formatString("gunHfjdkjdfIdfdf")); // Output: "HELLO"
 console.log(formatString("anoar hosen", true)); // Output: "HELLO"
 console.log(formatString("aFNAODFJDKF", false)); // Output: "hello"
 
+// problem -2
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const highRatedItems = items.filter((item) => item.rating >= 4);
+  return highRatedItems;
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 }
+];
+
+console.log(filterByRating(books));
+; 
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
 
